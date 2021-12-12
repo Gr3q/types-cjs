5e2fdffb0c604431ab853534bda4d7daee6fd6fa
jonath92 <jonathanheard92@gmail.com>
2021-12-12 19:25:55 +0100

    remove manual XApp declaration

--- XApp.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace imports.gi.XApp {
-  
-    // https://projects.linuxmint.com/xapp/reference/XAppGtkWindow.html
-    // A subclass of GtkWindow that allows additional communication with the window manager.
-    class GtkWindow extends imports.gi.Gtk.Window  {
-        constructor(options?: Partial<imports.gi.Gtk.WindowOptions>)
-    }
-}
